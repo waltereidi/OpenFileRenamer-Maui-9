26a0e6077f158c79310958ece82f6b4c744b211c
walter-eidi <walter-eidi@hotmail.com>
2025-03-16 21:01:17 -0300

    Iniciado implementação da interface de escolha de arquivos

---
+++ Presentation.Maui/Components/Layout/MainLayout.razor.cs
@@ -0,0 +1,24 @@
+﻿
+
+namespace Presentation.Maui.Components.Layout
+{
+    public partial class MainLayout
+    {
+
+        private DirectoryInfo CurrentDirectory { get; set; }
+        private string ChooseDirectoryButtonText
+            => CurrentDirectory != null
+            ? "Change Directory"
+            : "Choose Directory";
+
+        private string CurrentDirectoryText
+            => CurrentDirectory != null
+            ? this.CurrentDirectory.FullName
+            : "Choose a directory"; 
+
+
+
+
+
+    }
+}
