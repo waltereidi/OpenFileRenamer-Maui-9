47c99f610bfe0d5dcc790b24d5d4520c0c762bfc
walter-eidi <walter-eidi@hotmail.com>
2025-03-18 19:58:17 -0300

    Iniciado estudos de implementação para componentes com herança em razor

---
+++ Presentation.Maui/Shared/DataTableModel.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presentation.Maui.Shared
+{
+    public abstract class DataTableModel : ComponentBase
+    {
+        public readonly Guid Id;
+        public readonly Type type; 
+        public DataTableModel() 
+        { 
+            Id = new Guid();
+        }
+        private void BuildTable<T>()
+        {
+
+
+        }
+        public T GetData<T>()
+        {
+            throw new Exception();
+        }
+        protected override void OnParametersSet()
+        {
+
+        }
+
+
+
+    }
+}
