5976739857c951b1c56ad8e2cbe142d9fe8ae7e7
walter-eidi <walter-eidi@hotmail.com>
2025-03-21 20:07:36 -0300

    Adicionado componentes de input e wrappers

---
+++ Presentation.Maui/Components/Shared/FileFilter.razor.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Presentation.Maui.Components.Shared
+{
+    public partial class FileFilter : ComponentBase
+    {
+        public FileFilter() { }
+    }
+}
---
+++ Presentation.Maui/Components/Shared/Inputs/AlsoNumberedTextInput.razor.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presentation.Maui.Components.Shared.Inputs
+{
+    public partial class AlsoNumberedTextInput
+    {
+        private bool IsNumber { get; set; } = false;
+        public string Text { get => GetText(); }
+
+        public void ChangeToNumberTextInput() => IsNumber = true;
+        public void ChangeToTextInput() => IsNumber = false;    
+        public AlsoNumberedTextInput()
+        {
+
+        }
+        public string GetText()
+        {
+            long number = 0;
+            
+            if (!String.IsNullOrEmpty(Text) && long.TryParse(Text, out number) && IsNumber)
+                return number.ToString();
+            else if (IsNumber && number == 0)
+                return string.Empty; 
+            else
+                return Text;
+        }
+
+
+        [Parameter]
+        public string ClassName { get; set; }
+        [Parameter]
+        public string PlaceHolder { get; set; }
+        [Parameter]
+        public string AriaLabel { get; set; }
+        [Parameter]
+        public string AriaDescribedBy { get; set; }
+
+    }
+    
+}
