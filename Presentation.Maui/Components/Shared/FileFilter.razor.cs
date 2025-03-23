3ecece381ebc9f6b84e07969492ab5bb8698262b
walter-eidi <walter-eidi@hotmail.com>
2025-03-22 21:04:50 -0300

    adicionado interface maui, iniciado implementação de comunicação bidirecional entre componentes

--- Presentation.Maui/Components/Shared/FileFilter.razor.cs
+++ Presentation.Maui/Components/Shared/FileFilter.razor.cs
@@ -1,9 +1,13 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Presentation.Maui.DTO;
 
 namespace Presentation.Maui.Components.Shared
 {
     public partial class FileFilter : ComponentBase
     {
         public FileFilter() { }
+        
+
+
     }
 }
