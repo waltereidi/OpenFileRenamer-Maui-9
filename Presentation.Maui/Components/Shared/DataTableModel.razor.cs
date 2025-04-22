de44d67dc9f666e73344c098d7699ca6504df2aa
walter-eidi <walter-eidi@hotmail.com>
2025-04-21 21:01:24 -0300

    identificado um problema de paradigmas ao desenvolver no .net MAUI com moldes de framework javascript, iniciado refatoração para utilizar o potencial do blazor e C# melhor.

--- Presentation.Maui/Components/Shared/DataTableModel.razor.cs
+++ Presentation.Maui/Components/Shared/DataTableModel.razor.cs
@@ -1,73 +1,89 @@
 ﻿using Microsoft.AspNetCore.Components;
 using Presentation.Maui.Attributes;
+using Presentation.Maui.Service;
+using System.Linq.Expressions;
 using System.Reflection;
+using System.Threading.Tasks.Sources;
 
 
 namespace Presentation.Maui.Components.Shared
 {
     public partial class DataTableModel : ComponentBase
     {
-        private record ColumnDef(int index , MemberInfo originator , bool visible , string columnName , Guid ColumnId);
-        private readonly Guid Id;
-        private List<ColumnDef> _columns { get; set; }
-        
-        [Parameter]
-        public List<object> DataSource { get; set; }        
-        [Parameter]
-        public Type Type { get; set; }
-
-        private List<MemberInfo> GetPropertyMember()
-            => Type.GetMembers()
-                .Where(x => x.MemberType == MemberTypes.Property)
-                .ToList();
-
-        public DataTableModel() 
-        { 
-            Id = Guid.NewGuid();
-        }
-        
-        protected override Task OnParametersSetAsync()
+        private DirectoryInfo _dir { get => DirectoryManagerService._dir; }
+        public DataTableModel()
         {
-            RenderTable(); 
-            return base.OnParametersSetAsync();
-        }
-        
-        public void RenderTable()
-        {
-            if (Type == null)
-                throw new ArgumentNullException(nameof(Type));
-
-            _columns = GetColumnDefinition();
-
-            if (DataSource != null && DataSource.Any() && DataSource.First().GetType().Name != nameof(Type))
-                throw new InvalidDataException(nameof(DataSource));
-
-            if (DataSource != null && DataSource.Any())
-                RenderData();
-
+            if (!_dir.Exists)
+                Navigation.NavigateTo($"/");
         }
 
-        private void RenderData()
-        {
 
-        }
+        //    private record ColumnDef(int index , MemberInfo originator , bool visible , string columnName , Guid ColumnId);
+        //    private readonly Guid Id;
+        //    private List<ColumnDef> _columns { get; set; }
 
-        private List<ColumnDef> GetColumnDefinition()
-            => GetPropertyMember()
-                .Select((s, index) => new ColumnDef(index , s , IsColumnVisible(s) , GetColumnName(s), Guid.NewGuid()))
-                .ToList();
+        //    public List<object> DataSource = new();     
+        //    [Parameter]
+        //    public Type Type { get; set; }
+        //    [Parameter] 
+        //    public EventCallback<Type> TypeChanged { get; set; }
 
-        private string GetColumnName(MemberInfo m)
-            => !m.GetCustomAttributesData()
-                .Any(x => x.AttributeType.Name == nameof(DataTableColumnNameAttribute)) 
-                ?m.Name 
-                :m.GetCustomAttributesData()
-                        .First(x => x.AttributeType.Name == nameof(DataTableColumnNameAttribute))
-                        .ConstructorArguments.First().Value.ToString();
+        //    [Parameter] 
+        //    public Expression<Func<Type>> TypeExpression { get; set; }
+        //    private List<MemberInfo> GetPropertyMember()
+        //        => Type.GetMembers()
+        //            .Where(x => x.MemberType == MemberTypes.Property)
+        //            .ToList();
+        //    private async Task OnValueChanged(Type value)
+        //    => await this.TypeChanged.InvokeAsync(value);
+        //    public DataTableModel() 
+        //    { 
+        //        Id = Guid.NewGuid();
+        //    }
+
+        //    protected override Task OnParametersSetAsync()
+        //    {
+        //        RenderTable(); 
+        //        return base.OnParametersSetAsync();
+        //    }
+
+        //    public void RenderTable()
+        //    {
+        //        if (Type == null)
+        //            throw new ArgumentNullException(nameof(Type));
+
+        //        _columns = GetColumnDefinition();
+
+        //        if (DataSource != null && DataSource.Any() && DataSource.First().GetType().Name != nameof(Type))
+        //            throw new InvalidDataException(nameof(DataSource));
+
+        //        if (DataSource != null && DataSource.Any())
+        //            RenderData();
+
+        //    }
+
+        //    private void RenderData()
+        //    {
+
+        //    }
+
+        //    private List<ColumnDef> GetColumnDefinition()
+        //        => GetPropertyMember()
+        //            .Select((s, index) => new ColumnDef(index , s , IsColumnVisible(s) , GetColumnName(s), Guid.NewGuid()))
+        //            .ToList();
+
+        //    private string GetColumnName(MemberInfo m)
+        //        => !m.GetCustomAttributesData()
+        //            .Any(x => x.AttributeType.Name == nameof(DataTableColumnNameAttribute)) 
+        //            ?m.Name 
+        //            :m.GetCustomAttributesData()
+        //                    .First(x => x.AttributeType.Name == nameof(DataTableColumnNameAttribute))
+        //                    .ConstructorArguments.First().Value.ToString();
 
 
-        private bool IsColumnVisible(MemberInfo m)
-            => !m.GetCustomAttributesData()
-                .Any(x => x.AttributeType.Name == nameof(DataTableHiddenColumnAttribute));
+        //    private bool IsColumnVisible(MemberInfo m)
+        //        => !m.GetCustomAttributesData()
+        //            .Any(x => x.AttributeType.Name == nameof(DataTableHiddenColumnAttribute));
+        //}
     }
 }
---
+++ Presentation.Maui/Service/DirectoryManagerService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presentation.Maui.Service
+{
+    internal static class DirectoryManagerService
+    {
+        public static DirectoryInfo _dir { get; private set ; }
+        public static void SetDirectory(string dir)
+            => _dir = new DirectoryInfo(dir);
+
+    }
+}
