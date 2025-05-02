e30e1557052b35c659389a5138b9e1f809a55bdf
walter-eidi <walter-eidi@hotmail.com>
2025-05-02 20:58:52 -0300

    Adicionado git modules , Iniciado implementação de componente de preview maui

---
+++ Maui.Tests/Configuration.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Maui.Tests
+{
+    public class Configuration
+    {
+        public readonly DirectoryInfo _testDirPath;
+
+        public Configuration()
+        {
+            _testDirPath = new
+                (
+                    Path.Combine(
+                        Directory.GetParent(AppContext.BaseDirectory)
+                            .Parent.Parent.Parent.Parent.FullName, "Maui.Tests" , "TestFiles")
+                );
+        }
+
+    }
+}
---
+++ Maui.Tests/Presentation/DataSelectionDAOTest.cs
@@ -0,0 +1,34 @@
+﻿using ApplicationService.DAO;
+using ApplicationService.Interfaces;
+
+
+namespace Maui.Tests.Presentation
+{
+    [TestClass]
+    public class DataSelectionDAOTest : Configuration
+    {
+   
+        private ITableSelection Dao; 
+        public DataSelectionDAOTest()
+        {
+            var filter = new DataFilterDAO("", "Contains");
+            Dao = new TableSelectionDAO(_testDirPath , filter);
+        }
+
+        [TestMethod]
+        public void TestGetRowsReturnAFile()
+        {
+            var result = Dao.GetRows();
+            Assert.IsTrue(result.Count() > 0);
+        }
+        [TestMethod]
+        public void TestCheckRowRetunsFileUnchecked()
+        {
+            var result = Dao.GetRows();
+            var file = result.First(x => x.IsChecked);
+            Dao.CheckBoxChanged(file.FileIdentity , false);
+
+            Assert.IsTrue( Dao.GetRows().Count(x=>x.IsChecked == false) == 1 );
+        }
+    }
+}
