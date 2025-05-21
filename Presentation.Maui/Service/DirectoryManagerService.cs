45956de4fe81e67135b7940e295ce7c30cf184f0
walter-eidi <walter-eidi@hotmail.com>
2025-05-21 20:05:53 -0300

    Iniciado estudos de publicação da interface maui

--- Presentation.Maui/Service/DirectoryManagerService.cs
+++ Presentation.Maui/Service/DirectoryManagerService.cs
@@ -11,7 +11,9 @@ namespace Presentation.Maui.Service
             var result = await FolderPicker.Default.PickAsync();
             if (result.IsSuccessful)
                 _dir = new(result.Folder.Path);
+
         }
+
         public static bool IsValid() 
             =>_dir != null && _dir.Exists && _dir.GetFiles().Any();
         
